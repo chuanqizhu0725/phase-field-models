8c06abecc16f3eb2cc53978cea2af91ef6874f48
chuanqizhu0725 <chuanqizhu1991@163.com>
2022-03-09 09:23:19 +0800

    ..

--- TripleJunction/coupled-grain/main.c
+++ TripleJunction/coupled-grain/main.c
@@ -13,9 +13,9 @@
 #include <sstream>
 #include <typeinfo>
 
-#define NDX 200 //差分計算における計算領域一辺の分割数
+#define NDX 100 //差分計算における計算領域一辺の分割数
 #define NDY 100 //差分計算における計算領域一辺の分割数
-#define N 20    //考慮する結晶方位の数＋１(MPF0.cppと比較して、この値を大きくしている)
+#define N 4     //考慮する結晶方位の数＋１(MPF0.cppと比較して、この値を大きくしている)
 
 int ndx = NDX;
 int ndy = NDY;
@@ -29,6 +29,8 @@ double aij[N][N]; //勾配エネルギー係数
 double wij[N][N]; //ペナルティー項の係数
 double mij[N][N]; //粒界の易動度
 double fij[N][N]; //粒界移動の駆動力
+int anij[N][N];
+double thij[N][N];
 int phinum;
 
 int i, j, k, l, ii, jj, kk, ll, it; //整数
@@ -52,13 +54,23 @@ double delta;  //粒界幅（差分ブロック数にて表現）
 double mobi;   //粒界の易動度
 double vm0;    //モル体積
 
+double astre;
+double theta, theta0;
+double epsilon0;
+double termiikk, termjjkk;
+
+double phidx, phidy, phidxx, phidyy, phidxy;
+double ep, ep1p, ep2p;
+
 int x11, y11, x1h[10], y1h[10]; //初期核の座標
 double t, r0, r;
+
+double calcTheta(double dy, double dx);
 
 //******* メインプログラム ******************************************
 int main(int argc, char *argv[])
 {
-    nstep = 501;
+    nstep = 10001;
     dtime = 5.0;
     temp = 1000.0;
     L = 2000.0;
@@ -68,10 +80,11 @@ int main(int argc, char *argv[])
 
     dx = L / (double)NDX * 1.0e-9;       //差分プロック１辺の長さ(m)
     gamma0 = 0.5 * vm0 / RR / temp / dx; //粒界エネルギ密度（0.5J/m^2）を無次元化
+    astre = 0.03;
     A0 = 8.0 * delta * gamma0 / PI / PI; //勾配エネルギー係数[式(4.40)]
     W0 = 4.0 * gamma0 / delta;           //ペナルティー項の係数[式(4.40)]
     M0 = mobi * PI * PI / (8.0 * delta); //粒界の易動度[式(4.40)]
-    F0 = 80.0 / RR / temp;               //粒界移動の駆動力
+    F0 = 50.0 / RR / temp;               //粒界移動の駆動力
 
     for (i = 1; i <= nm; i++)
     {
@@ -81,9 +94,12 @@ int main(int argc, char *argv[])
             aij[i][j] = A0;
             mij[i][j] = M0;
             fij[i][j] = 0.0;
+            anij[i][j] = 0;
+            thij[i][j] = 0.0;
             if ((i == nm) || (j == nm))
             {
                 fij[i][j] = F0;
+                anij[i][j] = 1;
             }
             if (i > j)
             {
@@ -95,18 +111,12 @@ int main(int argc, char *argv[])
                 aij[i][j] = 0.0;
                 mij[i][j] = 0.0;
                 fij[i][j] = 0.0;
+                anij[i][j] = 0;
             }
         }
     }
-
-    // /************************* master code *******************************/
-    // if (taskid == MASTER)
-    // {
-    //     if (NX % numworkers != 0)
-    //     {
-    //         MPI_Abort(MPI_COMM_WORLD, rc);
-    //         exit(1);
-    //     }
+    thij[1][3] = PI / 8.0;
+    thij[3][1] = PI / 8.0;
 
     double phi[N][NDX][NDY], phi2[N][NDX][NDY]; //フェーズフィールド、フェーズフィールド補助配列
     int phiIdx[N][NDX][NDY];                    //位置(i,j)およびその周囲(i±1,j±1)において、pが０ではない方位の番号
@@ -136,23 +146,27 @@ int main(int argc, char *argv[])
         }
     }
 
-    r0 = 10.0;
-    for (ii = 1; ii <= nm - 1; ii++)
+    for (i = 0; i <= ndmx; i++)
     {
-        // x1=x1h[ii]; y1=y1h[ii];
-        x11 = rand() % 100 * (ndx / 100);
-        y11 = rand() % 100 * (ndy / 100); //初期核の位置
-
-        for (i = 0; i <= ndmx; i++)
+        for (j = 0; j <= ndmy; j++)
         {
-            for (j = 0; j <= ndmy; j++)
+            if (i > NDX * 4 / 5 && j < NDY / 2)
             {
-                r = sqrt(((i - x11)) * (i - x11) + (j - y11) * (j - y11));
-                if (r <= r0)
-                {
-                    phi[ii][i][j] = 1.0;
-                    phi[nm][i][j] = 0.0;
-                } //初期核位置のフェーズフィールドを設定
+                phi[1][i][j] = 1.0;
+                phi[2][i][j] = 0.0;
+                phi[3][i][j] = 0.0;
+            }
+            else if (i > NDX * 4 / 5 && j >= NDY / 2)
+            {
+                phi[1][i][j] = 0.0;
+                phi[2][i][j] = 1.0;
+                phi[3][i][j] = 0.0;
+            }
+            else
+            {
+                phi[1][i][j] = 0.0;
+                phi[2][i][j] = 0.0;
+                phi[3][i][j] = 1.0;
             }
         }
     }
@@ -191,12 +205,12 @@ start:;
             jm = j - 1;
             if (i == ndmx)
             {
-                ip = 0;
+                ip = ndmx;
             }
             if (i == 0)
             {
-                im = ndmx;
-            } //周期的境界条件
+                im = 0;
+            }
             if (j == ndmy)
             {
                 jp = 0;
@@ -236,11 +250,11 @@ start:;
             jm = j - 1;
             if (i == ndmx)
             {
-                ip = 0;
+                ip = ndmx;
             }
             if (i == 0)
             {
-                im = ndmx;
+                im = 0;
             } //周期的境界条件
             if (j == ndmy)
             {
@@ -262,9 +276,48 @@ start:;
                     for (n3 = 1; n3 <= phiNum[i][j]; n3++)
                     {
                         kk = phiIdx[n3][i][j];
-                        sum1 += 0.5 * (aij[ii][kk] - aij[jj][kk]) * (phi[kk][ip][j] + phi[kk][im][j] + phi[kk][i][jp] + phi[kk][i][jm] - 4.0 * phi[kk][i][j]) + (wij[ii][kk] - wij[jj][kk]) * phi[kk][i][j]; //[式(4.31)の一部]
+
+                        phidx = (phi[kk][ip][j] - phi[kk][im][j]) / 2.0; //フェーズフィールドの空間１階微分
+                        phidy = (phi[kk][i][jp] - phi[kk][i][jm]) / 2.0;
+                        phidxx = (phi[kk][ip][j] + phi[kk][im][j] - 2.0 * phi[kk][i][j]); //フェーズフィールドの空間２階微分
+                        phidyy = (phi[kk][i][jp] + phi[kk][i][jm] - 2.0 * phi[kk][i][j]);
+                        phidxy = (phi[kk][ip][jp] + phi[kk][im][jm] - phi[kk][im][jp] - phi[kk][ip][jm]) / 4.0;
+
+                        if (anij[ii][kk] == 1)
+                        {
+                            epsilon0 = sqrt(aij[ii][kk]);
+                            theta0 = thij[ii][kk];
+                            theta = calcTheta(phidy, phidx);                                    //界面の法線方向の角度[式(4.24)]
+                            ep = epsilon0 * (1.0 + astre * cos(4.0 * (theta - theta0)));        //勾配エネルギー係数の平方根[式(4.23)]
+                            ep1p = -epsilon0 * astre * 4.0 * sin(4.0 * (theta - theta0));       // epの角度による１階微分
+                            ep2p = -epsilon0 * astre * 4.0 * 4.0 * cos(4.0 * (theta - theta0)); // epの角度による２階微分
+
+                            termiikk = ep * ep * (phidxx + phidyy) + ep * ep1p * ((phidyy - phidxx) * sin(2.0 * theta) + 2.0 * phidxy * cos(2.0 * theta)) - 0.5 * (ep1p * ep1p + ep * ep2p) * (2.0 * phidxy * sin(2.0 * theta) - phidxx - phidyy - (phidyy - phidxx) * cos(2.0 * theta));
+                        }
+                        else
+                        {
+                            termiikk = aij[ii][kk] * (phi[kk][ip][j] + phi[kk][im][j] + phi[kk][i][jp] + phi[kk][i][jm] - 4.0 * phi[kk][i][j]);
+                        }
+
+                        if (anij[jj][kk] == 1)
+                        {
+                            epsilon0 = sqrt(aij[jj][kk]);
+                            theta0 = thij[jj][kk];
+                            theta = calcTheta(phidy, phidx);                                    //界面の法線方向の角度[式(4.24)]
+                            ep = epsilon0 * (1.0 + astre * cos(4.0 * (theta - theta0)));        //勾配エネルギー係数の平方根[式(4.23)]
+                            ep1p = -epsilon0 * astre * 4.0 * sin(4.0 * (theta - theta0));       // epの角度による１階微分
+                            ep2p = -epsilon0 * astre * 4.0 * 4.0 * cos(4.0 * (theta - theta0)); // epの角度による２階微分
+
+                            termjjkk = ep * ep * (phidxx + phidyy) + ep * ep1p * ((phidyy - phidxx) * sin(2.0 * theta) + 2.0 * phidxy * cos(2.0 * theta)) - 0.5 * (ep1p * ep1p + ep * ep2p) * (2.0 * phidxy * sin(2.0 * theta) - phidxx - phidyy - (phidyy - phidxx) * cos(2.0 * theta));
+                        }
+                        else
+                        {
+                            termjjkk = aij[jj][kk] * (phi[kk][ip][j] + phi[kk][im][j] + phi[kk][i][jp] + phi[kk][i][jm] - 4.0 * phi[kk][i][j]);
+                        }
+
+                        sum1 += 0.5 * (termiikk - termjjkk) + (wij[ii][kk] - wij[jj][kk]) * phi[kk][i][j];
                     }
-                    pddtt += -2.0 * mij[ii][jj] / double(phiNum[i][j]) * (sum1 - 8.0 / PI * fij[ii][jj] * sqrt(phi[ii][i][j] * phi[jj][i][j]));
+                    pddtt += -2.0 * mij[ii][jj] / double(phiNum[i][j]) * (sum1 - 8.0 / PI * fij[ii][jj] * (0.5 * i * i / NDX / NDX) * sqrt(phi[ii][i][j] * phi[jj][i][j]));
                     //フェーズフィールドの発展方程式[式(4.31)]
                 }
                 phi2[ii][i][j] = phi[ii][i][j] + pddtt * dtime; //フェーズフィールドの時間発展（陽解法）
@@ -316,4 +369,24 @@ start:;
 
 end:;
     return 0;
+}
+
+double calcTheta(double dy, double dx)
+{
+    if (dx != 0.0)
+    {
+        return atan(dy / dx);
+    }
+    else if (dx == 0.0 && dy > 0)
+    {
+        return PI / 2.0;
+    }
+    else if (dx == 0.0 && dy < 0)
+    {
+        return PI / (-2.0);
+    }
+    else
+    {
+        return 0;
+    }
 }
