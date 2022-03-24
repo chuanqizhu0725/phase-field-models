8bb2d84d5f5ed8c7256ae4933938bfd44aff7500
chuanqizhu0725 <chuanqizhu1991@163.com>
2022-03-24 21:37:28 +0800

    Able to do random and multiple seeds

---
+++ mpf-nd/mpf-2d-diffusion-random-seed/plot.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+ns = 1000
+nx = 64
+ny = 64
+step_arr = np.arange(0, ns*11, ns)
+for step in step_arr:
+    df = pd.read_csv(f"data/phi/2d{step}.csv", header=None)
+    arr = df[0].values
+    mat = arr.reshape(nx, ny)
+    plt.matshow(mat)
+    plt.savefig(f"figures/phi/2d{step}")
+    plt.close()
+
+    dfc = pd.read_csv(f"data/con/2d{step}.csv", header=None)
+    arrc = dfc[0].values
+    matc = arrc.reshape(nx, ny)
+    plt.matshow(matc)
+    plt.savefig(f"figures/con/2d{step}")
+    plt.close()
