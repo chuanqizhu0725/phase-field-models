d713d8c27ebde9269a407781b431c58456381780
chuanqizhu0725 <chuanqizhu1991@163.com>
2022-03-25 10:37:30 +0800

    Add temperature gradient

---
+++ mpf-nd/mpf-2d-diffusion-random-seed-temp-grad/plot0.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(f"data/con/2d0.csv", header=None)
+arr = df.values
+mat = arr.reshape(64, 64)
+plt.figure(figsize=(5, 5))
+# plt.plot(mat[:, 50])
+plt.matshow(mat)
+plt.show()
