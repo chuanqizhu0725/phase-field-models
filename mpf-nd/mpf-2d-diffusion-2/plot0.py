489f337c19b1f0f7de8959542cb76f3a251b6d31
chuanqizhu0725 <chuanqizhu1991@163.com>
2022-03-28 16:13:28 +0800

    Simplify correction

---
+++ mpf-nd/mpf-2d-diffusion-2/plot0.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(f"data/con/2d800.csv", header=None)
+arr = df.values
+mat = arr.reshape(64, 64)
+# plt.figure(figsize=(5, 5))
+plt.plot(mat[42, :])
+# plt.matshow(mat)
+plt.show()
